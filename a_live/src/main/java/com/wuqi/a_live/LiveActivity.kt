55d3a3a7fc995ceddaf60997db75602fb5a393b6
wq <wuqi@weyee.com>
2019-10-09 10:45:48 +0800

    webrtc半成品

---
+++ a_live/src/main/java/com/wuqi/a_live/LiveActivity.kt
@@ -0,0 +1,12 @@
+package com.wuqi.a_live
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LiveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_live)
+    }
+}
---
+++ middleware/libWSLive/src/main/java/me/lake/librestreaming/client/CallbackDelivery.java
@@ -0,0 +1,29 @@
+package me.lake.librestreaming.client;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+
+
+public class CallbackDelivery {
+    static private CallbackDelivery instance;
+    private final Executor mCallbackPoster;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+
+    public static CallbackDelivery i() {
+        return instance == null ? instance = new CallbackDelivery() : instance;
+    }
+
+    private CallbackDelivery() {
+        mCallbackPoster = handler::post;
+    }
+
+    public void post(Runnable runnable) {
+        mCallbackPoster.execute(runnable);
+    }
+
+    public void postDelayed(Runnable runnable, long time) {
+        handler.postDelayed(runnable,time);
+    }
+}
